a46d236a61bc6c812fb00c33fd9c250e89b325c1
godgroot <85842733+godgroot@users.noreply.github.com>
2021-10-04 13:20:12 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,7 +11,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Mᴀsᴛᴇʀ : <a href='https://t.me/COLD_ONEZ'>Cᴏʟᴅ々Oɴᴇᴢ࿐</a>\n○ Lᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ Lɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://github.com/COLD-ONEZ/File-SHare-Bot'>Click Here</a>\n○ Cʜᴀɴɴᴇʟ : @Mo_Tech_YT\n○ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ : @Mo_Tech_Group</b>",
+            text = f"<b>○ Mᴀsᴛᴇʀ : <a href='https://t.me/Im_godgroot'>ആശാൻ... Aashaan</a>\n○ Lᴀɴɢᴜᴀɢᴇ : <code>Python3</code>\n○ Lɪʙʀᴀʀʏ : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ : <a href='https://t.me/CINEMAZIP'>Click Here</a>\n○ Cʜᴀɴɴᴇʟ : @Cinemaziplinks\n○ Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ : @Cinemazip</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
